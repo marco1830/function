dd97b2506fc9596126f657485d1fe50553187c53
marco1830 <markoal1830@gmail.com>
2023-02-07 20:42:26 +0700

    tugasfunction

---
+++ function.js
@@ -0,0 +1,17 @@
+
+// tugas1
+function hitungLuasPersegiPanjang(panjang, lebar){
+    return panjang * lebar
+} 
+document.write('panjang: 10cm <br> lebar: 5cm <br>' )
+
+const luas = (hitungLuasPersegiPanjang(10, 5))
+document.write ('luas persegi panjang : ' +luas);
+
+//tugas2
+function volumeKubus(sisi, sisi, sisi){
+    return sisi*sisi*sisi
+}document.write()
+
+const hasilVolumeKubus = (volumeKubus(10, 10, 10))
+document.write('<br><br>Volume Kubus Adalah: ' +hasilVolumeKubus);
